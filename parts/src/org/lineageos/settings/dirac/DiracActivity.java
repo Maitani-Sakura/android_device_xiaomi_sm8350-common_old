CZB666-wdnmd/device_xiaomi_sm8350-common

sm8350-common: parts: Enable use_resource_processor for all sysui deps
This will make the build faster

Test: m checkbuild
Bug: 295208392
Fix for UQPR2

parts/src/org/lineageos/settings/dirac/DiracActivity.java
@@ -19,7 +19,6 @@ package org.lineageos.settings.dirac;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class DiracActivity extends CollapsingToolbarBaseActivity {
 
@@ -29,7 +28,7 @@ public class DiracActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getSupportFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new DiracSettingsFragment(), TAG_DIRAC).commit();
     }
 }

parts/src/org/lineageos/settings/thermal/ThermalSettingsActivity.java
@@ -19,7 +19,6 @@ package org.lineageos.settings.thermal;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class ThermalSettingsActivity extends CollapsingToolbarBaseActivity {
 
@@ -28,7 +27,7 @@ public class ThermalSettingsActivity extends CollapsingToolbarBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getSupportFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new ThermalSettingsFragment(), TAG_THERMAL).commit();
     }
 }

Maitani-Sakura/android_device_xiaomi_sm8350-common_old

Signed-off-by: RobertGarciaa <chae0218@naver.com>

parts/src/org/lineageos/settings/dirac/DiracActivity.java
@@ -19,7 +19,6 @@ package org.lineageos.settings.dirac;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class DiracActivity extends CollapsingToolbarBaseActivity {
 
@@ -29,7 +28,7 @@ public class DiracActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new DiracSettingsFragment(), TAG_DIRAC).commit();
     }
 }

parts/src/org/lineageos/settings/thermal/ThermalSettingsActivity.java
@@ -20,7 +20,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class ThermalSettingsActivity extends CollapsingToolbarBaseActivity {
 
@@ -29,7 +28,7 @@ public class ThermalSettingsActivity extends CollapsingToolbarBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new ThermalSettingsFragment(), TAG_THERMAL).commit();
     }
 